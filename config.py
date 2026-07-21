a7db202591bcab14219529d16e560e9f567f722e
Aleena Ayesha Alvi <ayeshaalvi2004aleena@gmail.com>
2026-07-21 18:48:13 +0000

    FINAL

---
+++ config.py
@@ -0,0 +1,9 @@
+EMAIL = "23f2004591@ds.study.iitm.ac.in"
+
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjIwMDQ1OTFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDY1NDEwNCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTI1ODkwNH0.3kH9JqRxjsbyTBqHsnCH3UHs_TuFhQp4f88eU65f3Fs"
+
+AIPIPE_BASE = "https://aipipe.org/openai/v1"
+
+TEXT_MODEL = "gpt-4o-mini"
+VISION_MODEL = "gpt-4o"
+EMBED_MODEL = "text-embedding-3-small"
